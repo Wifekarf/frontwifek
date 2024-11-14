a97b3701c04b9ea7977e2d7ec21ba5ff807e9e7b
Wifekarf <115703686+Wifekarf@users.noreply.github.com>
2024-11-14 12:47:17 +0100

    Update chambre-service.service.ts

--- src/app/service/chambre-service.service.ts
+++ src/app/service/chambre-service.service.ts
@@ -9,7 +9,7 @@ import { Chambre } from '../model/chambre.model/chambre.model.module';
   providedIn: 'root',
 })
 export class ChambreService {
-  private apiUrl = 'http://192.168.73.167:8088/tpfoyer/chambre';
+  private apiUrl = 'http://192.168.73.167:8088/tpfoyer17/api/chambre';
 
   constructor(private http: HttpClient) {}
 
